e97c32acfbfcdf9dcfdc1248346a7c9bb0bce6fd
Shandy <shandy@Shandy.local>
2022-07-13 12:41:31 +0200

    Get method works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { myKey, myToken } from './keys'; 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [boards, setBoards] = useState();
+
+  useEffect(
+    () => {
+      getBoards();
+    }, []
   );
+
+  async function getBoards() {
+    const res = await axios.get(
+       `https://api.trello.com/1/members/me/boards?fields=name,id,starred&key=${myKey}&token=${myToken}`
+    );
+    console.log(res.data);
+    setBoards(res.data);
+  }
+  
+  return (
+    <div>
+      <h1> Hello Trello </h1>
+        <ul>
+          {
+            boards && boards.map( b => 
+              <li id = {b.id}> { b.name } </li>)
+          }
+        </ul>
+
+    </div>    
+  )
 }
 
 export default App;
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+const myKey = 'b36e18cdc873573949ffa5b0e945c8e6';
+const myToken = '0ae12e4c0d3338fbaf20e192bf7d22ee35ca810e19e8d75d294eee0f97146f07';
+
+export {myKey, myToken}
